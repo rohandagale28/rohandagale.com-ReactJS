0cf1c2590cd65979f90085bb39eb297fcb34d753
rohandagale <workspacex28@gmail.com>
2023-09-18 14:33:26 +0530

    version 0.2.1

--- src/TechStackPage/TechStackPage.tsx
+++ src/TechStackPage/TechStackPage.tsx
@@ -6,7 +6,7 @@ export const TechStackPage = () => {
     return (
         <>
             <div className='techstack'>
-                <div style={{ position: "absolute", top: "6.4rem", fontSize: "1.4rem", textAlign: "center" }}>Programing Languages, Frameworks, Tools & Software</div>
+                <div style={{ position: "absolute", top: "6.4rem", fontSize: "1.4rem", textAlign: "center", color:"#999999" }}>Programing Languages, Frameworks, Tools & Software</div>
                 {Data.map((item) => {
                     return (
                         <React.Fragment key={item.id}>
